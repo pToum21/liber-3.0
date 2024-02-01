f5c44ab1edfe1a04b89a21d2452868c3aafa5595
mintyry <gitmintyry@gmail.com>
2024-02-01 15:03:45 -0800

    made schemas files with typeDefs and resolvers

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,20 @@
+const typeDefs = `
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+}
+
+type Query {
+
+}
+
+type Mutation {
+
+}
+
+`;
+
+module.exports = typeDefs;
