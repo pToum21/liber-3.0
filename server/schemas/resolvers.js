2a4b6e9c890fb844fcd425eb6793b9d04c3a60ae
pashag123 <pashag123@gmail.com>
2024-02-02 14:08:41 -0800

    resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,27 +1,56 @@
 const { User, Book } = require('../models');
-
+const { AuthenticationError } = require('../utils/auth')
 
 module.exports = {
+    
+    Query: {
+        myLibrary: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
+                return userData;
+            }
+            throw AuthenticationError;
+        },
+    },
+    
+    
+    
+    
     Mutation: {
-        createUser: async (parent, {email, password, username}) => {
+        createUser: async (parent, { email, password, username }) => {
             try {
                 const user = await User.create(args);
                 const token = signToken(user);
 
-                return{ user, token };
+                return { user, token };
 
             } catch (error) {
                 console.log(error);
                 throw new Error(error);
             }
         },
-       
-        
-        
+
+
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
     },
-    Query: {
-        
-    }
+   
 }
 
 
