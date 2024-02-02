959995c0214e8151350d50635eb5c324516cb316
toum21 <pey2moo1@gmail.com>
2024-02-02 14:17:51 -0800

    added a Reviews model

---
+++ server/models/Reviews.js
@@ -0,0 +1,21 @@
+const { model, Schema } = require('mongoose')
+const mongoose = require('mongoose')
+
+
+
+const commentsSchema = new Schema({
+    comment: String
+})
+
+const reviewSchema = new Schema({
+    // id is auto-generated via mongoose
+    rating: { type: Number, min: 0, max: 5, default: 0 },
+    comments: [commentsSchema],
+})
+
+
+
+
+const Review = model('Review', reviewSchema)
+
+module.exports = Review
