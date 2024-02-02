87fa28932a5cf7c673447089f673ebbe95c2fe84
mintyry <gitmintyry@gmail.com>
2024-02-01 22:52:34 -0800

    made type book and auth typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,6 +7,20 @@ type User {
     username: String
     email: String
     password: String
+    role: String
+}
+
+type Book {
+    _id: ID
+    title: String
+    authors: [String]
+    image: String
+    text: String
+}
+
+type Auth {
+    token: ID!
+    user: User
 }
 
 type Query {
