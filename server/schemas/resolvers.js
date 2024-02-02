8943c5632b5e4841c60aa85ece47c1e6c568f100
pashag123 <pashag123@gmail.com>
2024-02-02 13:28:55 -0800

    resolvers start

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,28 @@
-const { User } = require('../models');
+const { User, Book } = require('../models');
 
-const resolvers = {
-    Query: {
 
-    },
-
+module.exports = {
     Mutation: {
+        createUser: async (parent, {email, password, username}) => {
+            try {
+                const user = await User.create(args);
+                const token = signToken(user);
 
+                return{ user, token };
+
+            } catch (error) {
+                console.log(error);
+                throw new Error(error);
+            }
+        },
+       
+        
+        
+    },
+    Query: {
+        
     }
-};
+}
+
 
 module.exports = resolvers;
