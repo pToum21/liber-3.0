06470708359473a50871262a81a64c841de236e3
SalGonzalez151 <imdone151@gmail.com>
2024-02-02 18:55:58 -0800

    fixed type schema to mixed

--- server/models/User.js
+++ server/models/User.js
@@ -11,7 +11,7 @@ const userSchema = new Schema({
     password: String,
 
     keptBooks: [{
-        type: Schema.Types.ObjectId,
+        type: Schema.Types.Mixed,
         ref: 'Book'
     }]
 });
