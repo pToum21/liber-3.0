749db1ecb1375b5d52d3a38c23f2a8024b9ac1a8
toum21 <pey2moo1@gmail.com>
2024-02-03 23:56:34 -0800

    updated navbar to be fixed

---
+++ client/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,98 @@
+// import './navbar.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+
+const pages = ['Products', 'Pricing', 'Blog'];
+
+function Navbar() {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    return (
+        <AppBar position="static" sx={{ backgroundColor: '#161520' }}>
+            <Box maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        href="#app-bar-with-responsive-menu"
+                        id='liber'
+                        sx={{
+                            ml: 4,
+                            display: { xs: 'none', md: 'flex' },
+                            fontFamily: 'monospace',
+                            fontWeight: 700,
+                            letterSpacing: '.3rem',
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}
+                    >
+                        Liber
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: 'flex', justifyContent: 'flex-end' }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                        </IconButton>
+
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {/* Menu items go here */}
+                        </Menu>
+
+                    </Box>
+
+                    <Box sx={{ pr: 4 }}>
+                        <Button
+                            onClick={handleCloseNavMenu}
+                            sx={{ my: 2, color: 'white', fontFamily: 'YourSpecificFont' }}
+                        // Replace 'YourSpecificFont' with the desired font family
+                        >
+                            myLibrary
+                        </Button>
+                    </Box>
+
+                </Toolbar>
+            </Box>
+        </AppBar>
+    );
+}
+
+export default Navbar;
