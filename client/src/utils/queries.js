370242e538a0f402ed018ca90c4213005a61d86a
mintyry <gitmintyry@gmail.com>
2024-02-04 09:44:24 -0800

    exported QUERY_ALL_BOOKS

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,3 +1,33 @@
 // purely to add color to text in our code for readability
 import { gql } from '@apollo/client';
 
+// may need to refactor the reviews part, because we may be unnecessarily querying ids or user info
+export const QUERY_ALL_BOOKS = gql`
+query Query {
+    getBooks {
+      _id
+      title
+      bookId
+      authors {
+        name
+      }
+      image {
+        data
+      }
+      text
+      reviews {
+        _id
+        comments {
+          _id
+          comments
+          userId
+        }
+        rating
+        userId {
+          username
+          _id
+        }
+      }
+    }
+  }
+`;
