8ff132bde1593cebe2c46827a2f86469cdbf8fa2
toum21 <pey2moo1@gmail.com>
2024-02-04 12:27:05 -0800

    re designed the nav bar

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -3,68 +3,80 @@ import { AppBar, Toolbar, Typography, Button, IconButton, Menu, MenuItem } from 
 import MenuIcon from '@mui/icons-material/Menu';
 import { styled } from '@mui/system';
 import Hidden from '@mui/material/Hidden';
+import './navbar.css'
 
 const StyledAppBar = styled(AppBar)({
-  backgroundColor: '#2196f3', // You can customize the color
+    backgroundColor: '#161520',
+    fontFamily: 'Montserrat'
 });
 
 const StyledIconButton = styled(IconButton)({
-  marginRight: 2,
+    marginRight: 2,
+    fontFamily: 'Montserrat'
+});
+
+const TitleTypography = styled(Typography)({
+    marginRight: '50px',
+    fontSize: '1.8rem'
 });
 
 const StyledTypography = styled(Typography)({
-  flexGrow: 1,
+    flexGrow: 1,
+    display: 'flex',
+    alignItems: 'center',
+    fontFamily: 'Montserrat'
 });
 
 const NavBar = () => {
-  const [anchorEl, setAnchorEl] = React.useState(null);
+    const [anchorEl, setAnchorEl] = React.useState(null);
 
-  const handleMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
 
-  return (
-    <StyledAppBar position="sticky">
-      <Toolbar>
-        <Hidden lgUp>
-          {/* Display the hamburger icon only on screens smaller than lg */}
-          <StyledIconButton edge="start" color="inherit" aria-label="menu" onClick={handleMenuOpen}>
-            <MenuIcon />
-          </StyledIconButton>
-        </Hidden>
-        <StyledTypography variant="h6">
-          Book Reader
-        </StyledTypography>
+    return (
+        <StyledAppBar position="sticky">
+            <Toolbar>
+                <Hidden lgUp>
+                    <StyledIconButton edge="start" color="inherit" aria-label="menu" onClick={handleMenuOpen}>
+                        <MenuIcon />
+                    </StyledIconButton>
+                </Hidden>
 
-        <Hidden mdDown>
-          {/* Display these buttons on medium and larger screens */}
-          <Button color="inherit">Books</Button>
-          <Button color="inherit">Genres</Button>
-          <Button color="inherit">MyLibrary</Button>
-          <Button color="inherit">Log in</Button>
-        </Hidden>
+                <TitleTypography id="liber" variant="h6">
+                    Liber
+                </TitleTypography>
 
-        <Hidden lgUp>
-          {/* Display these buttons on large screens (lgUp) */}
-          <Menu
-            id="responsive-menu"
-            anchorEl={anchorEl}
-            open={Boolean(anchorEl)}
-            onClose={handleMenuClose}
-          >
-            <MenuItem onClick={handleMenuClose}>Books</MenuItem>
-            <MenuItem onClick={handleMenuClose}>Genres</MenuItem>
-            <MenuItem onClick={handleMenuClose}>MyLibrary</MenuItem>
-            <MenuItem onClick={handleMenuClose}>Log in</MenuItem>
-          </Menu>
-        </Hidden>
-      </Toolbar>
-    </StyledAppBar>
-  );
+                <StyledTypography variant="h6">
+                    <Button color="inherit" sx={{ fontFamily: 'Montserrat' }}>MyLibrary</Button>
+                </StyledTypography>
+
+                <Hidden mdDown>
+                    <Button color="inherit" sx={{ fontFamily: 'Montserrat' }}>Books</Button>
+                </Hidden>
+
+                <Hidden mdDown>
+                    <Button color="inherit" sx={{ fontFamily: 'Montserrat' }}>Log in</Button>
+                </Hidden>
+
+                <Hidden lgUp>
+                    <Menu
+                        id="responsive-menu"
+                        anchorEl={anchorEl}
+                        open={Boolean(anchorEl)}
+                        onClose={handleMenuClose}
+                    >
+                        <MenuItem onClick={handleMenuClose}>Books</MenuItem>
+                        <MenuItem onClick={handleMenuClose}>Log in</MenuItem>
+                    </Menu>
+                </Hidden>
+            </Toolbar>
+        </StyledAppBar>
+    );
 };
 
 export default NavBar;
