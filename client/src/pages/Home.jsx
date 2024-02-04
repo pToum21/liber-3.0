9a4ff4c913d145f52cb4c0b66fb652a3016da2ac
toum21 <pey2moo1@gmail.com>
2024-02-04 13:27:14 -0800

    updated the carousel

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,28 @@
 // this is likely where we will put div with scrolling books displaying
 
 // maybe even include bookshelf here, but when books are a dull/grey color, so then when user is signed in, they light up or become more vibrant and this will be their library?
+import React from 'react';
+import { Carousel, CarouselItem, Paper, Typography } from '@mui/material';
 
+
+const images = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+];
 
 function Home() {
     return (
-        <p>test</p>
+        <Carousel>
+            {images.map((image, index) => (
+                <CarouselItem key={index}>
+                    <Paper>
+                        <img src={image} alt={`slide-${index}`} style={{ width: '100%' }} />
+                    </Paper>
+                </CarouselItem>
+            ))}
+        </Carousel>
     );
-};
+}
 
 export default Home;
