01ce2f3b6f632f0387f42dfdf55453392c802b78
mintyry <gitmintyry@gmail.com>
2024-02-04 10:22:06 -0800

    modified main.jsx to use createBorwswerRouter

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Error from './pages/Error.jsx';
+// for routing process
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+// this establishes pages component structure and their paths
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    // these will be the children components and paths that will be injected into Outlet
+    children: [
+      // include objects here. each object will be a (url) path and element <>
+    ]
+  }
+]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
