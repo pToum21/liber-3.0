b58f70419ad0a345202b29fdbceee3c3a638ed1c
toum21 <pey2moo1@gmail.com>
2024-02-06 14:37:18 -0800

    in single book got the title and text to render

--- client/src/pages/SingleBook.jsx
+++ client/src/pages/SingleBook.jsx
@@ -1,11 +1,34 @@
 
+import { useQuery } from '@apollo/client'
+import { QUERY_ONE_BOOK } from '../utils/queries'
+import { useParams } from 'react-router-dom'
 
-const SingleBook = () => {
-    return (
-        <>
-        <h1>SingleBook</h1>
-        </>
-    )
+
+function SingleBook() {
+    const { id } = useParams();
+
+    const { loading, data } = useQuery(QUERY_ONE_BOOK, {
+        variables: { id: id}
+    });
+
+    const book = data?.getBooks || [];
+
+    console.log(data);
+
+    if (loading) {
+        return <div>Loading....</div>
     }
-    
-    export default SingleBook;
+
+
+
+    return (
+        <div >
+            <h3>book: {data.getSingleBook.title}</h3>
+            {/* <p style={{fontSize: '.7rem'}}>{data.getSingleBook.text}</p>*/}
+            
+        </div>
+    )
+}
+
+
+export default SingleBook;
