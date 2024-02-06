5367a73e8a27590d5c615a948e6b48a12be5ba7b
toum21 <pey2moo1@gmail.com>
2024-02-05 20:09:04 -0800

    created an animation so when the model is clicked the camera zooms into the book on the desk, from here we will render a book in that spot using css

--- client/src/Components/Three/Three.jsx
+++ client/src/Components/Three/Three.jsx
@@ -1,23 +1,28 @@
 import { Canvas } from '@react-three/fiber';
+import { PerspectiveCamera } from '@react-three/drei';
 import Experience from './Experience';
 
-
 function Light({ brightness, color }) {
-    <rectAreaLight
-        width={5}
-        height={5}
-        color={color}
-        intensity={brightness}
-    />
+    return (
+        <rectAreaLight
+            width={5}
+            height={5}
+            color={color}
+            intensity={brightness}
+        />
+    );
 }
+
 const Three = () => {
     return (
-        <Canvas>
-            <Light brightness={20} color={'#ff0000'} />
-            <Experience />
-        </Canvas>
-    )
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+            <Canvas style={{ width: '100%', height: '100%' }}>
+                {/* <PerspectiveCamera makeDefault position={[100, 150, 400 ]} /> */}
+                <Light brightness={20} color={'#ff0000'} />
+                <Experience />
+            </Canvas>
+        </div>
+    );
+};
 
-}
-
-export default Three
+export default Three;
