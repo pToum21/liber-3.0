3ef51488042ff148ee148f67e46c932875d15a27
mintyry <gitmintyry@gmail.com>
2024-02-06 21:38:17 -0800

    truncated title text in books div

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -126,7 +126,9 @@ function Home() {
                                         <Grid item key={book._id} xs={2.3}>
 
                                             <img style={{ width: '100%', height: '25vw' }} src={`data:image/jpg;base64,${book.image.data}`} />
-                                            {/* <p style={{ fontSize: '.5rem', textWrap: 'wrap'}}> {book.title}</p> */}
+                                            <p className="home-book-titles" style={{ fontSize: '0.8rem', textWrap: 'wrap'}}> 
+                                            {book.title}
+                                            </p>
 
                                         </Grid>
                                     ))}
