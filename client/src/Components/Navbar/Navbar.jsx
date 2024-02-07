beb50f0b7df7f56e11147729ba35b8735d4c5aa6
toum21 <pey2moo1@gmail.com>
2024-02-06 21:36:27 -0800

    when viewed on mobile and the hamburger is clicked then it changes to an x

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { Typography, Button, IconButton, Menu, MenuItem, Modal, TextField, Hidden, InputAdornment, Grid } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
-import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 import { styled } from '@mui/system';
 import Login from '../../pages/Login';
 import { Link } from 'react-router-dom';
 import './navbar.css';
-import Auth from '../../utils/auth'
+import Auth from '../../utils/auth';
 
 // Liber brand
 const TitleTypography = styled(Typography)({
@@ -20,11 +20,11 @@ const StyledTypography = styled(Typography)({
     marginLeft: '2rem',
 });
 
-// functionality for nav menu
 const NavBar = () => {
     const [anchorEl, setAnchorEl] = useState(null);
     const [showSearchBar, setShowSearchBar] = useState(false);
     const [isLoginModalOpen, setLoginModalOpen] = useState(false);
+    const [isMenuOpen, setMenuOpen] = useState(false);
 
     const logout = (event) => {
         event.preventDefault();
@@ -33,13 +33,12 @@ const NavBar = () => {
 
     const handleMenuOpen = (event) => {
         setAnchorEl(event.currentTarget);
-        // Set the menu anchor position to the top left corner
-        document.getElementById('responsive-menu').style.top = '0';
-        document.getElementById('responsive-menu').style.left = '0';
+        setMenuOpen(true);
     };
 
     const handleMenuClose = () => {
         setAnchorEl(null);
+        setMenuOpen(false);
     };
 
     const handleSearchButtonClick = () => {
@@ -58,96 +57,66 @@ const NavBar = () => {
 
     return (
         <>
-            {/* parent */}
             <Grid container py={2} id="nav-parent" style={{ backgroundColor: "transparent", display: "flex", justifyContent: "space-between", flexWrap: "wrap" }}>
-
-                {/* child 1 (nav and brand)*/}
                 <Grid id="child-1" item sx={{ display: "flex", marginLeft: '2rem' }}>
-
-                    {/* nav menu, which will replace StyledTypography */}
                     <Hidden mdUp>
-                        <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleMenuOpen}>
-                            <MenuIcon />
+                        <IconButton
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            onClick={isMenuOpen ? handleMenuClose : handleMenuOpen}
+                            sx={{ position: 'relative' }}
+                        >
+                            {isMenuOpen ? <CloseIcon /> : <MenuIcon />}
                         </IconButton>
                     </Hidden>
-                    {/* brand */}
                     <Link style={{ textDecoration: 'none', color: 'black' }} to="/">
                         <TitleTypography id="liber" sx={{ whiteSpace: 'nowrap', display: 'flex', alignItems: 'center' }}>
                             {'{'}&nbsp;&nbsp;L i b e r&nbsp;&nbsp;{'}'}
                         </TitleTypography>
-                    </ Link>
-
-                    {/* nav buttons*/}
+                    </Link>
                     <StyledTypography variant="h6">
                         <Hidden mdDown>
                             <Button className="navlinks" color="inherit">
                                 MyLibrary
                             </Button>
-
                             <Button className="navlinks" color="inherit">
                                 Books
                             </Button>
-
                             <Button className="navlinks" color="inherit" onClick={handleLoginClick}>
                                 Login
                             </Button>
-
                             <Button className="navlinks" color="inherit" onClick={logout}>
                                 Logout
                             </Button>
                         </Hidden>
                     </StyledTypography>
                 </Grid>
-
-                {/* child 2 / search bar */}
                 <Grid item id="searchbar" sx={{ display: "flex", alignItems: "center", marginRight: "2rem", justifyContent: "right" }}>
-                    <TextField
-                        variant="outlined"
-                        size="small"
-                        placeholder="Search"
-                        sx={{
-                            // this is not placeholder text, idk what it is
-                            '& .MuiOutlinedInput-root': {
-                                color: 'white',
-                            },
-                            // this is placeholder text and user input text
-                            '& .MuiInputBase-input': {
-                                color: 'black',
-                                fontFamily: 'Lato'
-                            },
-                            // outlined input outline
-                            '& .MuiOutlinedInput-notchedOutline': {
-                                borderColor: 'black',
-                            },
-                        }}
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <IconButton edge="end" color="inherit">
-                                        <SearchIcon sx={{ color: 'white', backgroundColor: '#8abbb1', borderRadius: '5px', padding: '4px' }} />
-                                    </IconButton>
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
-
-                    {/* hamburger menu items */}
                     <Hidden lgUp>
                         <Menu
                             id="responsive-menu"
                             anchorEl={anchorEl}
+                            anchorReference="anchorPosition"
+                            anchorPosition={{ top: 0, left: 0 }}
                             open={Boolean(anchorEl)}
                             onClose={handleMenuClose}
                             sx={{
                                 '& .MuiMenu-paper': {
-                                    width: '100%',
-                                    height: '100%',
+                                    marginTop: '15%',
+                                    width: '100vw',
+                                    height: '100vh',
                                     transformOrigin: 'top center',
                                     transform: 'translateX(0%) translateY(0%)',
+                                    backgroundColor: '#f3f3ec',
                                 },
                                 '& .MuiMenuItem-root': {
-                                    fontSize: '1.5rem', // Adjust the font size as needed
-                                    padding: '1rem', // Adjust the padding as needed
+                                    fontSize: '2rem',
+                                    padding: '1rem',
+                                    transition: 'color 0.3s ease',
+                                    '&:hover': {
+                                        color: '#8abbb1',
+                                    },
                                 },
                             }}
                         >
@@ -169,13 +138,11 @@ const NavBar = () => {
                         </Menu>
                     </Hidden>
                 </Grid>
-
                 <Modal open={isLoginModalOpen} onClose={handleLoginModalClose}>
                     <div>
                         <Login open={isLoginModalOpen} onClose={handleLoginModalClose} />
                     </div>
                 </Modal>
-
             </Grid>
         </>
     );
