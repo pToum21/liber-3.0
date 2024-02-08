e7ee5d125ae53aa3a517b25be0759c69a8033f5e
mintyry <gitmintyry@gmail.com>
2024-02-08 14:32:28 -0800

    rendered custom error message for comment when user is not signed in

--- client/src/Components/CommentForm/index.jsx
+++ client/src/Components/CommentForm/index.jsx
@@ -6,6 +6,7 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Rating from '@mui/material/Rating';
 import Box from '@mui/material/Box';
+import { Link } from 'react-router-dom';
 
 const CommentForm = ({ bookId }) => {
   const [commentText, setCommentText] = useState('');
@@ -73,7 +74,7 @@ const CommentForm = ({ bookId }) => {
           />
         </Box>
       </form>
-      {error && <p>Error: {error.message}</p>}
+      {error && <p>Error: Please <Link className="no-text-dec" to="/login">log in</Link> to add a comment.</p>}
     </div>
   );
 };
