2f521dce8a22e2d786be0fdb65aa4a2fa5fcb889
toum21 <pey2moo1@gmail.com>
2024-02-08 16:35:25 -0800

    added super basic styling to the highest rated book

--- client/src/Components/HighestRatedBook/HighRatedBook.jsx
+++ client/src/Components/HighestRatedBook/HighRatedBook.jsx
@@ -2,27 +2,42 @@ import React from 'react';
 import { useQuery } from '@apollo/client';
 import { styled } from '@mui/system';
 import Typography from '@mui/material/Typography';
-import Grid from '@mui/material/Grid';
 import { QUERY_HIGHEST_RATED_BOOK } from '../../utils/queries';
 
 const Container = styled('div')({
     width: '100vw',
-    height: '50vh',
+    height: '60vh',
     boxSizing: 'border-box',
     display: 'flex',
-    flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#8abbb1',
+    padding: theme => theme.spacing(2),
+    flexDirection: 'row', // default direction
+    '@media (max-width: 600px)': {
+        flexDirection: 'column', // switch to column on smaller screens
+    },
+});
+
+const LeftSide = styled('div')({
+    flex: 1,
+    textAlign: 'left',
+    padding: theme => theme.spacing(2),
+});
+
+const RightSide = styled('div')({
+    flex: 1,
+    textAlign: 'center',
+    padding: theme => theme.spacing(2),
 });
 
 const BookInfo = styled('div')({
     textAlign: 'center',
-    marginTop: theme => theme.spacing(2),
 });
 
 const BookImage = styled('img')({
     maxWidth: '100%',
-    maxHeight: '70vh', 
+    maxHeight: '70vh',
     marginTop: theme => theme.spacing(2),
 });
 
@@ -41,30 +56,42 @@ const HighestRatedBook = () => {
     const highestRatedBook = data.highestRatedBook;
 
     return (
-        <Container>
-            <BookInfo>
-                <Typography variant="h4" gutterBottom>
-                    Highest Rated Book
-                </Typography>
-                {highestRatedBook ? (
-                    <>
-                        <Typography variant="h6" gutterBottom>
-                            Title: {highestRatedBook.title}
-                        </Typography>
-                        <Typography variant="body1" gutterBottom>
-                            Authors: {highestRatedBook.authors.map((author) => author.name).join(', ')}
-                        </Typography>
+        <>
 
-                        <BookImage
-                            src={`data:image/jpg;base64,${highestRatedBook.image.data}`}
-                            alt="highest rated book"
-                        />
-                    </>
-                ) : (
-                    <Typography variant="body1">No highest-rated book found</Typography>
-                )}
-            </BookInfo>
-        </Container>
+            <Typography variant="h4" gutterBottom>
+                Highest Rated Book
+            </Typography>
+            <Container>
+
+                <LeftSide>
+                    <Typography variant="body1">
+                        The highest Rated book of the day is, {highestRatedBook.title} take a chance to read this book and enjoy it with our other users by reading now or save it for later. Feel free to leave ratings on your favorite book to have it added to the spotlight
+                    </Typography>
+                </LeftSide>
+                <RightSide>
+                    <BookInfo>
+                        {highestRatedBook ? (
+                            <>
+                                <Typography variant="h6" gutterBottom>
+                                    Title: {highestRatedBook.title}
+                                </Typography>
+                                <Typography variant="body1" gutterBottom>
+                                    Authors: {highestRatedBook.authors.map((author) => author.name).join(', ')}
+                                </Typography>
+                                <BookImage
+                                    src={`data:image/jpg;base64,${highestRatedBook.image.data}`}
+                                    alt="highest rated book"
+                                />
+                            </>
+                        ) : (
+                            <Typography variant="body1">No highest-rated book found</Typography>
+                        )}
+                    </BookInfo>
+                </RightSide>
+            </Container>
+
+        </>
+
     );
 };
 
