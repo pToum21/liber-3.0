1ca3d1447a475d5166e5166783b10242b5126c26
toum21 <pey2moo1@gmail.com>
2024-02-09 15:23:17 -0800

    added someplace holder text in the footer

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Container, Grid } from "@mui/material";
-import gif from '../../../public/3dModels/output-onlinegiftools.gif'
+import gif from "../../../public/3dModels/output-onlinegiftools.gif";
 
 export const Footer = () => {
     const currentYear = new Date().getFullYear();
@@ -10,23 +10,27 @@ export const Footer = () => {
             sx={{
                 width: "100%",
                 height: "auto",
-                backgroundColor: "secondary.main",
                 paddingTop: "1rem",
                 paddingBottom: "1rem",
             }}
         >
             <Container maxWidth="lg">
-                <Grid container direction="column" alignItems="center">
-                    <img src={gif} alt="3d book gif" />
-                    <Grid item xs={12}>
-                        <h2 style={{ color: "black", fontSize: "1.5rem" }}>
-                            React Starter App
-                        </h2>
+                <Grid container alignItems="center">
+                    <Grid item xs={12} sm={6}>
+                        {/* Adjust the width and height for the desired size */}
+                        <img src={gif} alt="3d book gif" style={{ width: "18%", height: "auto" }} />
                     </Grid>
-                    <Grid item xs={12}>
-                        <p style={{ color: "textSecondary", fontSize: "1rem" }}>
-                            {`${currentYear} | React | Material UI | React Router`}
-                        </p>
+                    <Grid item xs={12} sm={6}>
+                        <Grid container direction="column" alignItems="center">
+                            <Grid item xs={12}>
+                                <h2 id ="liber" style={{ color: "black", fontSize: "1.5rem" }}>Liber</h2>
+                            </Grid>
+                            <Grid item xs={12}>
+                                <p style={{ color: "textSecondary", fontSize: "1rem" }}>
+                                    {`${currentYear} | Ryan | Peyton | Sal | Pasha`}
+                                </p>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </Container>
