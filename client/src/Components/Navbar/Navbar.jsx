67d342b9e577d0ca9d2552bd343d9aafb0a32ac2
mintyry <gitmintyry@gmail.com>
2024-02-11 09:04:04 -0800

    fixed mui outline of searchbar in nav

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -8,6 +8,9 @@ import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 import { styled } from '@mui/system';
 import Login from '../Login/Login';
+import { createTheme, ThemeProvider, useTheme } from '@mui/material/styles';
+// for outline of textfield
+import { outlinedInputClasses } from '@mui/material/OutlinedInput';
 // to link to other pages
 import { Link } from 'react-router-dom';
 // our files
@@ -88,7 +91,76 @@ const NavBar = () => {
     if (token) {
         const authUser = Auth.getProfile();
         role = authUser.data.role;
-    }
+    };
+
+// modify outline of textfield
+const customTheme = (outerTheme) =>
+createTheme({
+  palette: {
+    mode: outerTheme.palette.mode,
+  },
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          '--TextField-brandBorderColor': '#E0E3E7',
+          '--TextField-brandBorderHoverColor': '#B2BAC2',
+          '--TextField-brandBorderFocusedColor': '#6F7E8C',
+          '& label.Mui-focused': {
+            color: 'var(--TextField-brandBorderFocusedColor)',
+          },
+        },
+      },
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        notchedOutline: {
+          borderColor: 'var(--TextField-brandBorderColor)',
+        },
+        root: {
+          [`&:hover .${outlinedInputClasses.notchedOutline}`]: {
+            borderColor: 'var(--TextField-brandBorderHoverColor)',
+          },
+          [`&.Mui-focused .${outlinedInputClasses.notchedOutline}`]: {
+            borderColor: 'var(--TextField-brandBorderFocusedColor)',
+          },
+        },
+      },
+    },
+    MuiFilledInput: {
+      styleOverrides: {
+        root: {
+          '&::before, &::after': {
+            borderBottom: '2px solid var(--TextField-brandBorderColor)',
+          },
+          '&:hover:not(.Mui-disabled, .Mui-error):before': {
+            borderBottom: '2px solid var(--TextField-brandBorderHoverColor)',
+          },
+          '&.Mui-focused:after': {
+            borderBottom: '2px solid var(--TextField-brandBorderFocusedColor)',
+          },
+        },
+      },
+    },
+    MuiInput: {
+      styleOverrides: {
+        root: {
+          '&::before': {
+            borderBottom: '2px solid var(--TextField-brandBorderColor)',
+          },
+          '&:hover:not(.Mui-disabled, .Mui-error):before': {
+            borderBottom: '2px solid var(--TextField-brandBorderHoverColor)',
+          },
+          '&.Mui-focused:after': {
+            borderBottom: '2px solid var(--TextField-brandBorderFocusedColor)',
+          },
+        },
+      },
+    },
+  },
+});
+
+const outerTheme = useTheme();
 
     return (
         <>
@@ -156,7 +228,7 @@ const NavBar = () => {
                     </StyledTypography>
                 </Grid>
                 <Grid item id="searchbar" sx={{ display: "flex", alignItems: "center", marginRight: "2rem", justifyContent: "right", }}>
-
+                <ThemeProvider theme={customTheme(outerTheme)}>
                     <TextField
                         className="input-override"
                         variant="outlined"
@@ -199,6 +271,7 @@ const NavBar = () => {
                             ),
                         }}
                     />
+                    </ThemeProvider>
 
                     <Hidden lgUp>
                         <Menu
