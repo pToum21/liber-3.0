def4f6800b5c539a7aa662e9a85e0c64b40ebfd6
toum21 <pey2moo1@gmail.com>
2024-02-13 15:18:29 -0800

    when the screen is on mobile the letters component is hidden

--- client/src/Components/Three/Experience.jsx
+++ client/src/Components/Three/Experience.jsx
@@ -1,20 +1,34 @@
-import { OrbitControls } from '@react-three/drei'
-import BookShelf from './Bookshelf'
-import GrassPlain from './Grassplain'
-import Letters from './LiberLetters'
-
+import { OrbitControls } from '@react-three/drei';
+import { useThree } from '@react-three/fiber';
+import { useState, useEffect } from 'react';
+import BookShelf from './Bookshelf';
+import GrassPlain from './Grassplain';
+import Letters from './LiberLetters';
 
 const Experience = () => {
+    const { size } = useThree();
+    const [showLetters, setShowLetters] = useState(true);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setShowLetters(size.width > 768); 
+        };
+
+        handleResize(); 
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, [size.width]);
+
     return (
-        // this renders the entire 3d scene which is then used inside of Three
         <>
             <OrbitControls />
             <ambientLight />
-            <Letters />
+            {showLetters && <Letters />}
             <BookShelf />
             <GrassPlain />
         </>
-    )
-}
+    );
+};
 
 export default Experience;
