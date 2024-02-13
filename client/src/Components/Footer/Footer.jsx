3ca608d09c04295628fdd324f949c3886c085693
toum21 <pey2moo1@gmail.com>
2024-02-12 20:28:13 -0800

    working on reformatting footer

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -15,8 +15,8 @@ export const Footer = () => {
       }}
     >
       <Container maxWidth="lg">
-        <Grid container alignItems="center">
-          <Grid item xs={12} sm={6}>
+        <Grid  alignItems="center" style={{display: 'flex', justifyContent: 'space-between'}}>
+          <Grid item xs={12} sm={6} >
 
             <img src={logo} alt="elegant book logo" style={{ width: "20vw", height: "auto" }} />
           </Grid>
@@ -25,16 +25,23 @@ export const Footer = () => {
               container
               direction="column"
               alignItems="center"
-              justifyContent="center"
+              // justifyContent="center"
               style={{ height: "100%" }}
             >
               <Grid item xs={12}>
                 <span style={{ fontSize: '2rem', fontFamily: 'Coventry Garden', whiteSpace: 'nowrap' }}>{'{'} L i b e r {'}'}</span>
               </Grid>
-              <Grid item xs={12}>
-                <p style={{ color: "textSecondary", fontSize: "1.2rem", margin: 0 }}>
-                  {`Copyright © ${currentYear} | Ryan | Peyton | Sal | Pasha`}
+              <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'center', flexDirection: 'column' }}>
+                <div style={{display: 'flex', justifyContent: 'center'}}>
+                <p style={{ color: "textSecondary", fontSize: "1.1rem", margin: 0 }}>
+                  Copyright © {currentYear}
                 </p>
+                </div>
+                
+                <div>
+                  <p style={{ color: "textSecondary", fontSize: "1.3rem", margin: 0 }}> Ryan | Peyton | Sal | Pasha</p>
+                </div>
+
               </Grid>
             </Grid>
           </Grid>
